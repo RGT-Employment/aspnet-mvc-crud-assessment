5331652e0222bf50bdb6444a855ecef040dac796
BRIAN <BRIAN.MARTI@NEOSOFT.PT>
2024-03-15 17:20:47 -0700

    Bug Fixes - Done with some bug fix on LaunchController.cs and Edit.cshtml Optimize - Done in LaunchController.cs for EditAction and DeleteAction Enhance - Added new login feature for user identification (The Username and Password can be anything as of now)            -New files added      *UserAccountController      *Login.cshtml      *LoginModel.cs Impacted files - Web.config

---
+++ CrudMVCCodeFirst/Models/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudMVCCodeFirst.Models
+{
+    public class LoginModel
+    {
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+    }
+}
